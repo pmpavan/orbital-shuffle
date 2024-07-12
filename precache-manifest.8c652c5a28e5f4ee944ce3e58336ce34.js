022c304e3daaf11f70982ac6771654672459cc39
Pavan P M <pmpavan@outlook.com>
2024-07-12 13:21:45 +0530

    Updates

---
+++ precache-manifest.8c652c5a28e5f4ee944ce3e58336ce34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92fb12b5385271c2bbee27c573b75979",
+    "url": "/orbital-shuffle/index.html"
+  },
+  {
+    "revision": "a16bb2d36251553b0237",
+    "url": "/orbital-shuffle/static/css/main.4dd44e2c.chunk.css"
+  },
+  {
+    "revision": "c93bc4298778182cd639",
+    "url": "/orbital-shuffle/static/js/2.02a9664d.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/orbital-shuffle/static/js/2.02a9664d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4917e5640163dd21b1dc",
+    "url": "/orbital-shuffle/static/js/3.038e7856.chunk.js"
+  },
+  {
+    "revision": "a16bb2d36251553b0237",
+    "url": "/orbital-shuffle/static/js/main.7ec4d65f.chunk.js"
+  },
+  {
+    "revision": "64c55ababf67449b8163",
+    "url": "/orbital-shuffle/static/js/runtime-main.1a401d24.js"
+  }
+]);
